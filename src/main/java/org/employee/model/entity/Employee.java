8e80c9a37e8af6dc9afd6e5caa08b56f80b27661
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-24 22:30:33 +0530

    creating attributes of Employee

--- src/main/java/org/employee/model/entity/Employee.java
+++ src/main/java/org/employee/model/entity/Employee.java
@@ -1,9 +1,15 @@
 package org.employee.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String empName;
+    private String empEmail;
+    private String empCode;
+    private String companyName;
 }
