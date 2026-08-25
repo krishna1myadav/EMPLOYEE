8a5219076ce02c721a306deb85ab5f0649079770
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-25 23:46:35 +0530

    creating a new EmployeeRepository interface under the repository package

--- src/main/java/org/employee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/org/employee/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,44 @@
 package org.employee.service.impl;
 
-public class EmployeeServiceImpl {
+import org.employee.model.dto.EmployeeDto;
+import org.employee.repository.EmployeeRepository;
+import org.employee.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+        return null;
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+
+    }
+
+    @Override
+    public EmployeeDto getSingleEmployee(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        return List.of();
+    }
 }
