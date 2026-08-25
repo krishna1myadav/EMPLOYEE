44b03555ab30b71a5704e039c9033a1b57aa1b51
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-25 23:36:10 +0530

    adding the field of EmployeeService Interface

--- src/main/java/org/employee/service/EmployeeService.java
+++ src/main/java/org/employee/service/EmployeeService.java
@@ -1,4 +1,17 @@
 package org.employee.service;
 
+import org.employee.model.dto.EmployeeDto;
+
+import java.util.List;
+
 public interface EmployeeService {
+    EmployeeDto saveEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto);
+
+    void deleteEmployee(Long id);
+
+    EmployeeDto getSingleEmployee(Long id);
+
+    List<EmployeeDto> getAllEmployees();
 }
