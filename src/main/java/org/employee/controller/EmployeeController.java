57ed21dc23e401efa74d3556bae7da1038173096
krishan1myadav <krishna1myadav.@gmail.com>
2026-08-28 23:37:44 +0530

    creating two endpoints inside the EmployeeController Class

--- src/main/java/org/employee/controller/EmployeeController.java
+++ src/main/java/org/employee/controller/EmployeeController.java
@@ -1,4 +1,27 @@
 package org.employee.controller;
 
+import org.employee.model.dto.EmployeeDto;
+import org.employee.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees ")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto response = employeeService.saveEmployee(employeeDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 }
