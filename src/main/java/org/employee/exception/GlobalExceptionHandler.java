45a4bc71db94c61af47219c3fab30fbbcd4d5110
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-03 23:48:23 +0530

    created a new handleResourceNotFoundException method

--- src/main/java/org/employee/exception/GlobalExceptionHandler.java
+++ src/main/java/org/employee/exception/GlobalExceptionHandler.java
@@ -1,8 +1,16 @@
 package org.employee.exception;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), ex.getStatus());
+        return new ResponseEntity<>(response, ex.getStatus());
+    }
 }
