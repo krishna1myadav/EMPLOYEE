e84adca428034070e3f7e896170ee940fed95984
krishan1myadav <krishna1myadav.@gmail.com>
2026-09-05 23:50:28 +0530

    generated getter for message and status

--- src/main/java/org/employee/exception/MissingParameterException.java
+++ src/main/java/org/employee/exception/MissingParameterException.java
@@ -7,5 +7,17 @@ public class MissingParameterException extends RuntimeException{
     private String message;
     private HttpStatus status;
 
+    public MissingParameterException(String message){
+        this.message = message;
+        this.status = HttpStatus.BAD_REQUEST;
+    }
 
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
 }
